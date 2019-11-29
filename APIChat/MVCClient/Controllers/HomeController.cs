b456870a559472022aff6ef2a23447818a08c922
Ronald Ovalle <rrom2999@gmail.com>
2019-11-28 18:02:19 -0600

    Vistas login y creacion

--- APIChat/MVCClient/Controllers/HomeController.cs
+++ APIChat/MVCClient/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCClient.Models;
 
 namespace MVCClient.Controllers
 {
@@ -24,6 +25,28 @@ namespace MVCClient.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult VerificacionUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult VerificacionUsuario(string name, string lastname, string username, string password)
+        {
+            Usuario aCrear = new Usuario();
+            aCrear.Nombre = name; aCrear.Apellido = lastname; aCrear.Username = username; aCrear.Password = password;
+            return View();
+        }
+
+        public ActionResult CreacionUser()
+        {
             return View();
         }
     }
---
+++ APIChat/MVCClient/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCClient.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Username { get; set; }
+        public string  Password { get; set; }
+    }
+}
